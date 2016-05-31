bc56b1845fc8e5d203704a4d0d4eb85c3d18716b
Adam Luchini and Katarina Karolcikova <katarina@>
2016-05-31 15:06:12 -0700

    It works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(function() {
-  $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
+  $(".clickable1").click(function() {
+    $("body").removeClass();
+    $("#initially-showing1").toggle();
+    $("#initially-hidden1").toggle();
+    // $("#initially-showing2").toggle();
+    // $("#initially-hidden2").toggle();
+
+  });
+});
+$(function() {
+  $(".clickable2").click(function() {
+    $("body").removeClass();
+    $("#initially-showing2").toggle();
+    $("#initially-hidden2").toggle();
   });
 });
